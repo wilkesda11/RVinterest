3e24f8adb089d95180443209113786becc1e0072
David Wilkes <wilkesda11@gmail.com>
2014-07-14 21:03:08 -0400

    Initial Commit for RVinteresting

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rvinterest::Application.config.secret_key_base = '2eb74658f476f55ef6fcf7e780c5981fdfe5daecf0094086136223b93d5a782be6a33d78cfd26bf3339eb41d71e725e6eb6125b464f66d3c0da993ec476c4c94'
